9e28dbe1726b1a34c3642505629c799943dfd812
daishek <daichekkal@gmail.com>
2022-06-06 21:35:34 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 const App = () => {
-  const activeMenu = false
+  const activeMenu = true
   return (
     <div>
       <BrowserRouter>
